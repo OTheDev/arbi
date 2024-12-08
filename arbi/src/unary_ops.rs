04696e5a7fc18aa4f98962c30027a7936d635fc6
Owain Davies <116417456+OTheDev@users.noreply.github.com>
2024-12-09 00:16:17 +0700

    Add docs for the `Neg` trait (#41)

--- arbi/src/unary_ops.rs
+++ arbi/src/unary_ops.rs
@@ -5,9 +5,20 @@ SPDX-License-Identifier: Apache-2.0 OR MIT
 
 use crate::Arbi;
 
+/// Negate `self` in-place and return `self`.
+///
+/// # Examples
+/// ```
+/// use arbi::Arbi;
+/// let a = Arbi::from(123456789);
+/// let b = -a;
+/// assert_eq!(b, -123456789);
+/// ```
+///
+/// # Complexity
+/// \\( O(1) \\)
 impl core::ops::Neg for Arbi {
     type Output = Self;
-
     fn neg(mut self) -> Self {
         if self.size() != 0 {
             self.neg = !self.neg;
@@ -16,9 +27,20 @@ impl core::ops::Neg for Arbi {
     }
 }
 
+/// Return a new `Arbi` integer representing `self` negated.
+///
+/// # Examples
+/// ```
+/// use arbi::Arbi;
+/// let a = Arbi::from(123456789);
+/// let b = -&a; // Cloning occurs
+/// assert_eq!(b, -123456789);
+/// ```
+///
+/// # Complexity
+/// \\( O(n) \\)
 impl core::ops::Neg for &Arbi {
     type Output = Arbi;
-
     fn neg(self) -> Arbi {
         if self.size() != 0 {
             let ret = self.clone();
@@ -38,7 +60,6 @@ mod tests {
         let a = Arbi::from(10);
         let b = Arbi::from(-10);
         let z = Arbi::zero();
-
         assert_eq!(-a, -10);
         assert_eq!(-b, 10);
         assert_eq!(-z, 0);
@@ -49,7 +70,6 @@ mod tests {
         let a = Arbi::from(10);
         let b = Arbi::from(-10);
         let z = Arbi::zero();
-
         assert_eq!(-(&a), -10);
         assert_eq!(-(&b), 10);
         assert_eq!(-(&z), 0);
