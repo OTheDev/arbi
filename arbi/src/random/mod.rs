116b8171c9ef71f0b193a48954acf56c08de948e
OTheDev <116417456+OTheDev@users.noreply.github.com>
2024-12-26 09:59:23 +0700

    initial

---
+++ arbi/src/random/mod.rs
@@ -0,0 +1,10 @@
+/*
+Copyright 2024 Owain Davies
+SPDX-License-Identifier: Apache-2.0 OR MIT
+*/
+
+#![cfg(feature = "rand")]
+
+mod random_arbi;
+
+pub use random_arbi::RandomArbi;
---
+++ arbi/src/random/uniform_sampler.rs
@@ -0,0 +1,4 @@
+/*
+Copyright 2024 Owain Davies
+SPDX-License-Identifier: Apache-2.0 OR MIT
+*/
