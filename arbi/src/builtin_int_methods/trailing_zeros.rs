fe3bf6868177d36c3db84fb02ff347397fb50553
OTheDev <116417456+OTheDev@users.noreply.github.com>
2025-01-11 08:44:51 +0700

    Use consistent wording in trailing_zeros() doc

--- arbi/src/builtin_int_methods/trailing_zeros.rs
+++ arbi/src/builtin_int_methods/trailing_zeros.rs
@@ -6,7 +6,7 @@ SPDX-License-Identifier: Apache-2.0 OR MIT
 use crate::{Arbi, BitCount, Digit};
 
 impl Arbi {
-    /// If the integer is nonzero, returns the number of trailing zeros in the
+    /// If this integer is nonzero, returns the number of trailing zeros in the
     /// binary representation (two's complement) of `self`. Otherwise, returns
     /// `None`.
     ///
