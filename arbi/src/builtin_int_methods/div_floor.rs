8f3f8aeec8aef2d1e8ca0066348eb4d75342966c
OTheDev <116417456+OTheDev@users.noreply.github.com>
2025-07-22 12:06:27 +0700

    Implement divrem_floor

---
+++ arbi/src/builtin_int_methods/div_floor.rs
@@ -0,0 +1,78 @@
+/*
+Copyright 2025 Owain Davies
+SPDX-License-Identifier: Apache-2.0 OR MIT
+*/
+
+use crate::Arbi;
+
+impl Arbi {
+    /// Divide `self` by `rhs`, returning both a quotient and remainder:
+    /// `(q, r)`.
+    ///
+    /// The quotient is rounded towards negative infinity and the remainder will
+    /// have the same sign as `rhs`.
+    ///
+    /// # Panic
+    /// Panics if `rhs` is zero.
+    ///
+    /// # Examples
+    /// ```
+    /// use arbi::Arbi;
+    /// let (a, b) = (Arbi::from(8), Arbi::from(3));
+    /// let (ma, mb) = (a.negate_ref(), b.negate_ref());
+    ///
+    /// let (q, r) = a.divrem_floor_ref(&b);
+    /// assert_eq!(q, 2);
+    /// assert_eq!(r, 2);
+    ///
+    /// let (q, r) = a.divrem_floor_ref(&mb);
+    /// assert_eq!(q, -3);
+    /// assert_eq!(r, -1);
+    ///
+    /// let (q, r) = ma.divrem_floor_ref(&b);
+    /// assert_eq!(q, -3);
+    /// assert_eq!(r, 1);
+    ///
+    /// let (q, r) = ma.divrem_floor_ref(&mb);
+    /// assert_eq!(q, 2);
+    /// assert_eq!(r, -2);
+    ///
+    /// let (q, r) = Arbi::zero().divrem_floor_ref(&a);
+    /// assert_eq!(q, 0);
+    /// assert_eq!(r, 0);
+    ///
+    /// let (q, r) = Arbi::zero().divrem_floor_ref(&ma);
+    /// assert_eq!(q, 0);
+    /// assert_eq!(r, 0);
+    /// ```
+    ///
+    /// Panics if `rhs` is zero:
+    /// ```should_panic
+    /// use arbi::Arbi;
+    ///
+    /// let x = Arbi::from(123456789);
+    /// let (q, r) = x.divrem_floor_ref(&Arbi::zero());
+    /// ```
+    pub fn divrem_floor_ref(&self, rhs: &Self) -> (Self, Self) {
+        // Compute quotient and remainder, treating operands as nonnegative,
+        // with same semantics of / and % for builtin integers
+        let (mut q, mut r) = (Arbi::zero(), Arbi::zero());
+        Self::ddivide(&mut q, &mut r, &self.vec, &rhs.vec, false, false);
+
+        // Adjust for signs
+        if rhs.is_negative() {
+            r = -r;
+        }
+        match (self.signum(), rhs.signum()) {
+            (1, 1) | (-1, -1) | (0, 1) | (0, -1) => (q, r),
+            (1, -1) | (-1, 1) => {
+                if r.is_zero() {
+                    (-q, r)
+                } else {
+                    (-q - 1, rhs - r)
+                }
+            }
+            _ => unreachable!(),
+        }
+    }
+}
